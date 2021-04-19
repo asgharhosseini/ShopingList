104df9cb964db26a139537fed57a953df477ef28
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-19 11:55:48 +0430

    create ShoppingViewModel.kt for get data form repositories

---
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/viewmodel/ShoppingViewModel.kt
@@ -0,0 +1,51 @@
+package ir.vbile.app.shopinglist.ui.viewmodel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ir.vbile.app.shopinglist.data.model.ImageResponse
+import ir.vbile.app.shopinglist.data.model.ShoppingItem
+import ir.vbile.app.shopinglist.data.repositories.ShoppingRepository
+import ir.vbile.app.shopinglist.other.Event
+import ir.vbile.app.shopinglist.other.Resource
+
+import kotlinx.coroutines.launch
+
+class ShoppingViewModel @ViewModelInject constructor(
+    private val repository: ShoppingRepository
+) : ViewModel() {
+
+    val shoppingItems = repository.observeAllShoppingItems()
+
+    val totalPrice = repository.observeTotalPrice()
+
+    private val _images = MutableLiveData<Event<Resource<ImageResponse>>>()
+    val images: LiveData<Event<Resource<ImageResponse>>> = _images
+
+    private val _curImageUrl = MutableLiveData<String>()
+    val curImageUrl: LiveData<String> = _curImageUrl
+
+    private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> = _insertShoppingItemStatus
+
+    fun setCurImageUrl(url: String) {
+        _curImageUrl.postValue(url)
+    }
+
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.deleteShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.insertShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+
+    }
+
+    fun searchForImage(imageQuery: String) {
+
+    }
+}
