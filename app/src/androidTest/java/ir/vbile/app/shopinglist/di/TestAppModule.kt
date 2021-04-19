db39158fb650116414c73c0ca388659a5da3612d
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-19 14:20:04 +0430

    create TestAppModule.kt fro provide required dependency and add database dependency provider

---
+++ app/src/androidTest/java/ir/vbile/app/shopinglist/di/TestAppModule.kt
@@ -0,0 +1,23 @@
+package ir.vbile.app.shopinglist.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import ir.vbile.app.shopinglist.data.local.ShoppingItemDatabase
+import javax.inject.Named
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object TestAppModule{
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ShoppingItemDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
