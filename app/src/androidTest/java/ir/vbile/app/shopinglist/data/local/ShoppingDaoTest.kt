5aa8b76fb9fae0ffc5e03be20c03ae8c706a4793
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-19 14:20:45 +0430

    update ShoppingDaoTest.kt with inject dependency database

--- app/src/androidTest/java/ir/vbile/app/shopinglist/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/ir/vbile/app/shopinglist/data/local/ShoppingDaoTest.kt
@@ -7,6 +7,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import ir.vbile.app.shopinglist.data.model.ShoppingItem
 import ir.vbile.app.shopinglist.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,25 +18,28 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
-    private lateinit var database: ShoppingItemDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
     private lateinit var dao: ShoppingDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-                ApplicationProvider.getApplicationContext(),
-                ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
+        hiltRule.inject()
         dao = database.shoppingDao()
     }
 
