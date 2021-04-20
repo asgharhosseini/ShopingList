59812c235e0f8a2bd98c308447fba16b1ae1e44f
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 12:18:55 +0430

    set back navigate in AddShoppingItemFragment.kt

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragment.kt
@@ -1,10 +1,13 @@
 package ir.vbile.app.shopinglist.ui.fragment
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import android.os.*
+import android.view.*
+import androidx.activity.*
+import androidx.fragment.app.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
 import ir.vbile.app.shopinglist.R
-import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import ir.vbile.app.shopinglist.ui.viewmodel.*
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
 
 
 class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
@@ -14,5 +17,20 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        ivShoppingImage.setOnClickListener {
+            findNavController().navigate(
+                    AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                viewModel.setCurImageUrl("")
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+
+
     }
 } 
