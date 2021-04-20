3a6431dba3fb52a08f7bc08c4d060e00f6c99d91
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 14:21:32 +0430

    crate ImagePickFragmentTest.kt for test click Image  popBack Stack And SetImageUrl

---
+++ app/src/androidTest/java/ir/vbile/app/shopinglist/ui/fragment/ImagePickFragmentTest.kt
@@ -0,0 +1,71 @@
+package ir.vbile.app.shopinglist.ui.fragment
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import ir.vbile.app.shopinglist.R
+import ir.vbile.app.shopinglist.data.repositoreis.FakeShoppingRepositoryAndroidTest
+import ir.vbile.app.shopinglist.getOrAwaitValue
+import ir.vbile.app.shopinglist.launchFragmentInHiltContainer
+import ir.vbile.app.shopinglist.ui.adapter.ImageAdapter
+import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import javax.inject.Inject
+
+
+@HiltAndroidTest
+@MediumTest
+@ExperimentalCoroutinesApi
+class ImagePickFragmentTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun clickImage_popBackStackAndSetImageUrl() {
+        val navController = mock(NavController::class.java)
+        val imageUrl = "TEST"
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<ImagePickFragment>(fragmentFactory = fragmentFactory) {
+            Navigation.setViewNavController(requireView(), navController)
+            imageAdapter.images = listOf(imageUrl)
+            viewModel = testViewModel
+        }
+        onView(withId(R.id.rvImages)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ImageAdapter.ImageViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        verify(navController).popBackStack()
+        assertThat(testViewModel.curImageUrl.getOrAwaitValue()).isEqualTo(imageUrl)
+
+    }
+
+
+}
