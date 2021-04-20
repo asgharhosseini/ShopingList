77caed33b5654f65c1c14943dd65761b4883285d
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:25:43 +0430

    subscribe To Observers data and show in fragment

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragment.kt
@@ -1,25 +1,43 @@
 package ir.vbile.app.shopinglist.ui.fragment
-import android.os.*
-import android.view.*
-import androidx.activity.*
-import androidx.fragment.app.*
-import androidx.lifecycle.*
-import androidx.navigation.fragment.*
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import ir.vbile.app.shopinglist.R
-import ir.vbile.app.shopinglist.ui.viewmodel.*
+import ir.vbile.app.shopinglist.other.Status
+import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+import javax.inject.Inject
 
-
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+@AndroidEntryPoint
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
         ivShoppingImage.setOnClickListener {
             findNavController().navigate(
-                    AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
             )
         }
 
@@ -32,5 +50,35 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
         requireActivity().onBackPressedDispatcher.addCallback(callback)
 
 
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Added Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "An unknown error occcured",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.LOADING -> {
+                        /* NO-OP */
+                    }
+                }
+            }
+        })
     }
 } 
