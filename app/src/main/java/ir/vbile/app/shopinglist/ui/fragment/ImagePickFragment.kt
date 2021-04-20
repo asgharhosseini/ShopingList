3855183e5daf2d3b1bc97374588fb455795d6011
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:24:49 +0430

    subscribe To Observers data

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ImagePickFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ImagePickFragment.kt
@@ -1,18 +1,29 @@
 package ir.vbile.app.shopinglist.ui.fragment
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import ir.vbile.app.shopinglist.R
 import ir.vbile.app.shopinglist.other.Constants.GRID_SPAN_COUNT
+import ir.vbile.app.shopinglist.other.Constants.SEARCH_TIME_DELAY
+import ir.vbile.app.shopinglist.other.Status
 import ir.vbile.app.shopinglist.ui.adapter.ImageAdapter
 import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_image_pick.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class ImagePickFragment @Inject constructor(
     val imageAdapter: ImageAdapter
 ) : Fragment(R.layout.fragment_image_pick) {
@@ -24,11 +35,49 @@ class ImagePickFragment @Inject constructor(
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
 
         setupRecyclerView()
+        subscribeToObservers()
 
+        var job: Job? = null
+        etSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(SEARCH_TIME_DELAY)
+                editable?.let {
+                    if (editable.toString().isNotEmpty()) {
+                        viewModel.searchForImage(editable.toString())
+                    }
+                }
+            }
+        }
         imageAdapter.setOnItemClickListener {
             findNavController().popBackStack()
             viewModel.setCurImageUrl(it)
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.images.observe(viewLifecycleOwner, Observer {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        val urls = result.data?.hits?.map { imageResult -> imageResult.previewURL }
+                        imageAdapter.images = urls ?: listOf()
+                        progressBar.visibility = View.GONE
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "An unknown error occured.",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        progressBar.visibility = View.GONE
+                    }
+                    Status.LOADING -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
     }
 
     private fun setupRecyclerView() {
