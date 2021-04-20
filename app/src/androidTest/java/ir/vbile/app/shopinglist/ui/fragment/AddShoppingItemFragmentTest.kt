8eb044830a1002c9544070320071d3a3d2cdf8e8
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:28:54 +0430

    create function test for click Insert In to Db shopping Item Inserted In to Db

--- app/src/androidTest/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/ir/vbile/app/shopinglist/ui/fragment/AddShoppingItemFragmentTest.kt
@@ -1,13 +1,30 @@
 package ir.vbile.app.shopinglist.ui.fragment
 
-import androidx.navigation.*
-import androidx.test.espresso.Espresso.*
-import androidx.test.filters.*
-import dagger.hilt.android.testing.*
-import ir.vbile.app.shopinglist.*
-import kotlinx.coroutines.*
-import org.junit.*
-import org.mockito.Mockito.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import ir.vbile.app.shopinglist.R
+import ir.vbile.app.shopinglist.data.model.ShoppingItem
+import ir.vbile.app.shopinglist.data.repositoreis.FakeShoppingRepositoryAndroidTest
+import ir.vbile.app.shopinglist.getOrAwaitValue
+import ir.vbile.app.shopinglist.launchFragmentInHiltContainer
+import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -17,15 +34,41 @@ class AddShoppingItemFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: TestShoppingFragmentFactory
+
     @Before
     fun setup() {
         hiltRule.inject()
     }
 
     @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
+    }
+
+    @Test
     fun pressBackButton_popBackStack() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<AddShoppingItemFragment> {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
             Navigation.setViewNavController(requireView(), navController)
         }
 
