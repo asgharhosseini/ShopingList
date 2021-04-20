a5ac035dfbe23f81268fe0acae00842ad28f8330
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 14:16:56 +0430

    generate ImageAdapter in ImagePickFragment.kt

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ImagePickFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ImagePickFragment.kt
@@ -3,16 +3,38 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import ir.vbile.app.shopinglist.R
+import ir.vbile.app.shopinglist.other.Constants.GRID_SPAN_COUNT
+import ir.vbile.app.shopinglist.ui.adapter.ImageAdapter
 import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import kotlinx.android.synthetic.main.fragment_image_pick.*
+import javax.inject.Inject
 
 
-class ImagePickFragment : Fragment(R.layout.fragment_image_pick) {
+class ImagePickFragment @Inject constructor(
+    val imageAdapter: ImageAdapter
+) : Fragment(R.layout.fragment_image_pick) {
 
     lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+        setupRecyclerView()
+
+        imageAdapter.setOnItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setCurImageUrl(it)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        rvImages.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(requireContext(), GRID_SPAN_COUNT)
+        }
     }
 }
