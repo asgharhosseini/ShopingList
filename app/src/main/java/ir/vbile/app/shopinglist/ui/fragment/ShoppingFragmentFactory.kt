cadfeceb9672793978f5ba298cb0f69e1ee04e1a
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:17:06 +0430

    add AddShoppingItemFragment and ShoppingFragment to ShoppingFragmentFactory.kt

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragmentFactory.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragmentFactory.kt
@@ -2,16 +2,22 @@ package ir.vbile.app.shopinglist.ui.fragment
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.bumptech.glide.RequestManager
 import ir.vbile.app.shopinglist.ui.adapter.ImageAdapter
+import ir.vbile.app.shopinglist.ui.adapter.ShoppingItemAdapter
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
-    private val imageAdapter: ImageAdapter
+    private val imageAdapter: ImageAdapter,
+    private val glide: RequestManager,
+    private val shoppingItemAdapter: ShoppingItemAdapter
 ) : FragmentFactory() {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
+            AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
+            ShoppingFragment::class.java.name -> ShoppingFragment(shoppingItemAdapter)
             else -> super.instantiate(classLoader, className)
         }
     }
