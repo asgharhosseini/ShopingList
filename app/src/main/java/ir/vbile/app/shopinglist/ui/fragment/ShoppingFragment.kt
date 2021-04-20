488fd420094fed44749b80502c2b66aa7a414fb5
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 12:18:27 +0430

    set navigate in ShoppingFragment.kt

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragment.kt
@@ -1,11 +1,13 @@
 package ir.vbile.app.shopinglist.ui.fragment
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import android.os.*
+import android.view.*
+import androidx.fragment.app.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
 import ir.vbile.app.shopinglist.R
-import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
+import ir.vbile.app.shopinglist.ui.viewmodel.*
+import kotlinx.android.synthetic.main.fragment_shopping.*
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
 
@@ -14,5 +16,10 @@ class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        fabAddShoppingItem.setOnClickListener {
+            findNavController().navigate(
+                    ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+            )
+        }
     }
 }
