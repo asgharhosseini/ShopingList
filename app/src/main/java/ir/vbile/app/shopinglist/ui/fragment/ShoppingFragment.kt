10b7692282b59c4de4500da8970390bb626576ab
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:23:40 +0430

    inject shoppingItemAdapter and viewModel and subscribe To Observers  data and generate shoppingItemAdapter and create itemTouchCallback for delete item

--- app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragment.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/fragment/ShoppingFragment.kt
@@ -1,25 +1,83 @@
 package ir.vbile.app.shopinglist.ui.fragment
 
-import android.os.*
-import android.view.*
-import androidx.fragment.app.*
-import androidx.lifecycle.*
-import androidx.navigation.fragment.*
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.LEFT
+import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import ir.vbile.app.shopinglist.R
-import ir.vbile.app.shopinglist.ui.viewmodel.*
+import ir.vbile.app.shopinglist.ui.adapter.ShoppingItemAdapter
+import ir.vbile.app.shopinglist.ui.viewmodel.ShoppingViewModel
 import kotlinx.android.synthetic.main.fragment_shopping.*
+import javax.inject.Inject
 
-class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+@AndroidEntryPoint
+class ShoppingFragment @Inject constructor(
+    val shoppingItemAdapter: ShoppingItemAdapter,
+    var viewModel: ShoppingViewModel? = null
+) : Fragment(R.layout.fragment_shopping) {
 
-    lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        viewModel =
+            viewModel ?: ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+        setupRecyclerView()
         fabAddShoppingItem.setOnClickListener {
             findNavController().navigate(
-                    ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+                ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
             )
         }
     }
+
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, LEFT or RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.layoutPosition
+            val item = shoppingItemAdapter.shoppingItems[pos]
+            viewModel?.deleteShoppingItem(item)
+            Snackbar.make(requireView(), "Successfully deleted item", Snackbar.LENGTH_LONG).apply {
+                setAction("Undo") {
+                    viewModel?.insertShoppingItemIntoDb(item)
+                }
+                show()
+            }
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel?.shoppingItems?.observe(viewLifecycleOwner, Observer {
+            shoppingItemAdapter.shoppingItems = it
+        })
+        viewModel?.totalPrice?.observe(viewLifecycleOwner, Observer {
+            val price = it ?: 0f
+            val priceText = "Total Price: $price€"
+            tvShoppingItemPrice.text = priceText
+        })
+    }
+
+    private fun setupRecyclerView() {
+        rvShoppingItems.apply {
+            adapter = shoppingItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
+        }
+    }
 }
+
