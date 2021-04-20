35fc31f7b2a8d474d044d6e8ad5246b9dbd49fb0
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-20 15:17:35 +0430

    add support Fragment Manager to MainActivity.kt

--- app/src/main/java/ir/vbile/app/shopinglist/ui/MainActivity.kt
+++ app/src/main/java/ir/vbile/app/shopinglist/ui/MainActivity.kt
@@ -1,12 +1,20 @@
 package ir.vbile.app.shopinglist.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import ir.vbile.app.shopinglist.R
+import ir.vbile.app.shopinglist.ui.fragment.ShoppingFragmentFactory
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
     }
 }
